0bd59d1b5b166afae44c9827e3263bd6a54d3cc0
MauryaD <ankushrawat950@gmail.com>
2019-01-04 21:30:06 +0530

    displaying food items from database

--- app/src/main/java/com/alliance/foodintern/adapter/FoodItemsAdapter.java
+++ app/src/main/java/com/alliance/foodintern/adapter/FoodItemsAdapter.java
@@ -1,31 +1,51 @@
 package com.alliance.foodintern.adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.alliance.foodintern.R;
+import com.alliance.foodintern.model.FoodData;
+import com.bumptech.glide.Glide;
+import java.util.ArrayList;
 
 public class FoodItemsAdapter extends RecyclerView.Adapter<FoodItemsAdapter.myViewHolder> {
+    private Context mCtx;
+    private ArrayList<FoodData> foodList;
 
+    public FoodItemsAdapter(Context mCtx, ArrayList<FoodData> foodList) {
+        this.mCtx = mCtx;
+        this.foodList = foodList;
+    }
 
     @NonNull
     @Override
     public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(mCtx).inflate(R.layout.food_items, parent, false);
+        return new myViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull myViewHolder holder, int position) {
+        FoodData data=foodList.get(position);
+        Glide.with(mCtx)
+                .load(data.getImage())
+                .into(holder.imageView);
+        holder.discount.setText(data.getDiscount());
+        holder.description.setText(data.getDescription());
+        holder.name.setText(data.getFoodName());
+        holder.price.setText(data.getPrice());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return foodList.size();
     }
 
     public class myViewHolder extends RecyclerView.ViewHolder{
@@ -40,8 +60,6 @@ public class FoodItemsAdapter extends RecyclerView.Adapter<FoodItemsAdapter.myVi
             description=itemView.findViewById(R.id.desc);
             price=itemView.findViewById(R.id.price);
             discount=itemView.findViewById(R.id.disc);
-
-
         }
 
 
--- app/src/main/java/com/alliance/foodintern/fragment/FoodFragment.java
+++ app/src/main/java/com/alliance/foodintern/fragment/FoodFragment.java
@@ -4,7 +4,11 @@ package com.alliance.foodintern.fragment;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +19,15 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.alliance.foodintern.R;
+import com.alliance.foodintern.adapter.FoodItemsAdapter;
+import com.alliance.foodintern.model.FoodData;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import static android.support.v7.widget.TintTypedArray.obtainStyledAttributes;
 
@@ -28,6 +41,10 @@ public class FoodFragment extends Fragment {
         // Required empty public constructor
     }
 
+    DatabaseReference databaseReference;
+    RecyclerView recyclerView;
+    FoodItemsAdapter mAdapter;
+    ArrayList<FoodData> foodDataArrayList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,10 +55,43 @@ public class FoodFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        foodDataArrayList=new ArrayList<>();
+        recyclerView = view.findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        mAdapter= new FoodItemsAdapter(getContext(),foodDataArrayList);
+        recyclerView.setAdapter(mAdapter);
 
+        databaseReference= FirebaseDatabase.getInstance().getReference("Foods");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshots) {
+//                progressBar.setVisibility(View.GONE);
+                if (dataSnapshots.exists()) {
+                    for (DataSnapshot dataSnapshot : dataSnapshots.getChildren()) {
+                        String name = dataSnapshot.child("Name").getValue(String.class);
+                        String desc = dataSnapshot.child("Description").getValue(String.class);
+                        String image = dataSnapshot.child("Image").getValue(String.class);
+                        String price = dataSnapshot.child("Price").getValue(String.class);
+                        String discount = dataSnapshot.child("Discount").getValue(String.class);
+                        String menuId = dataSnapshot.child("MenuId").getValue(String.class);
+                        Log.e("asdghjklkj",name);
+                        foodDataArrayList.add(new FoodData(discount,name,desc,price,image,menuId));
+                    }
 
+                }
+                mAdapter.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
+
+    }
 
 
 }
