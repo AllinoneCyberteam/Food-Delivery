aafbd2669b8fd0699e32c6f4bbb106a14fc4c28c
shankar137 <shankard151@gmail.com>
2019-01-08 20:51:47 +0530

    Basic concept of Google  Maps with current location

--- app/src/main/java/com/alliance/foodintern/fragment/NoificationFragment.java
+++ app/src/main/java/com/alliance/foodintern/fragment/NoificationFragment.java
@@ -1,8 +1,18 @@
 package com.alliance.foodintern.fragment;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,27 +21,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.alliance.foodintern.MapsActivity;
 import com.alliance.foodintern.R;
+import com.alliance.foodintern.activity.MainActivity;
 import com.alliance.foodintern.adapter.CardItemAdapter;
 import com.alliance.foodintern.adapter.SqlDataBaseAdapter;
 import com.alliance.foodintern.model.CardItem;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
+
 public class NoificationFragment extends Fragment {
     RecyclerView mCardRecycler;
     ArrayList<CardItem> mCursorList;
     CardItemAdapter mCardItemAdapter;
     TextView st,dis,tax,mtotal;
     Button order;
-
+    private static final int ERROR_REQUEST = 9001;
+    private static final String TAG ="TAG" ;
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    public static final int MY_PERMISSIONS_REQUEST_COERSE = 98;
 
 
     public NoificationFragment() {
@@ -58,6 +78,99 @@ public class NoificationFragment extends Fragment {
         mCardRecycler.setHasFixedSize(true);
         mCardRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
         mCardRecycler.setAdapter(mCardItemAdapter);
+
+        order.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+          getCurrentLocation();
+                //Uri mapUri = Uri.parse("geo:0,0?q="+latitude+","+longitude);
+
+            }
+
+            String longitude,latitude;
+                private void getCurrentLocation() {
+
+                    LocationManager locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+                    LocationListener locationListener = new MyLocationListener();
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(getActivity(),
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                MY_PERMISSIONS_REQUEST_LOCATION);
+                        ActivityCompat.requestPermissions(getActivity(),
+                                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                MY_PERMISSIONS_REQUEST_COERSE);
+                    }
+
+                    if (locationManager != null) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+                    }
+                }
+
+                 class MyLocationListener implements LocationListener
+                {
+
+
+                    @Override
+                    public void onLocationChanged(android.location.Location loc) {
+
+                         longitude = "Longitude: " + loc.getLongitude();
+                        Log.v(TAG, longitude);
+                         latitude = "Latitude: " + loc.getLatitude();
+                        Log.v(TAG, latitude);
+
+                        /*------- To get city name from coordinates -------- */
+                        String cityName = null;
+
+                        try {
+                            Locale pname = Locale.getDefault();
+                            if (pname != null) {
+                                Geocoder gcd = new Geocoder(getContext(), pname);
+                                List<Address> addresses;
+                                addresses = gcd.getFromLocation(loc.getLatitude(),
+                                        loc.getLongitude(), 1);
+                                if (addresses.size() > 0) {
+                                    System.out.println(addresses.get(0).getLocality());
+                                    cityName = addresses.get(0).getSubLocality();
+
+                                }
+                            }
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        String s = longitude + "\n" + latitude + "\n\nMy Current City is: " + cityName;
+                        Toast.makeText(getContext(), ""+s, Toast.LENGTH_SHORT).show();
+                        Intent mapIntent = new Intent(getActivity(), MapsActivity.class);
+                        mapIntent.putExtra("lat",latitude);
+                        mapIntent.putExtra("lon",longitude);
+                        mapIntent.putExtra("name",cityName);
+                        //mapIntent.setPackage("com.google.android.apps.maps");
+                        startActivity(mapIntent);
+
+
+                    }
+
+
+
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                }
+
+        });
+
 
 
         db.open();
