e6ef7d3209074f1ef4db095be29668b2624eccf8
Ankush <ankushrawat950@gmail.com>
2019-01-20 19:41:52 +0530

    added Login Register activity

---
+++ app/src/main/java/com/alliance/foodintern/activity/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.alliance.foodintern.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alliance.foodintern.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
